9788570e316c9ba42efdf716305e6de61ca41b10
keeks-mtl <66472161+keeks-mtl@users.noreply.github.com>
2021-10-31 15:22:55 +0000

    Add value to card.js

--- project-app/src/components/UI/Cards/Card.js
+++ project-app/src/components/UI/Cards/Card.js
@@ -9,6 +9,8 @@ import api from "../../Api/Api";
 const GameCard = ({ hero }) => {
   const [heroData, setHeroData] = useState([]);
   const [ready, setReady] = useState(false);
+  const [value, setValue] = useState(null);
+
   useEffect(() => {
     const fetchApi = async () => {
       const { data } = await api.get(hero);
@@ -17,6 +19,11 @@ const GameCard = ({ hero }) => {
     };
     fetchApi();
   }, [hero]);
+
+  const handleClick = (e) => {
+    const { value } = e.target;
+    setValue(value);
+  };
 
   return ready ? (
     <Card sx={{ maxWidth: 250, maxHeight: 600 }}>
@@ -38,36 +45,48 @@ const GameCard = ({ hero }) => {
         <Button
           sx={{ backgroundColor: "purple", marginBottom: 2 }}
           variant="contained"
+          value={heroData.powerstats.combat}
+          onClick={handleClick}
         >
           {`Combat: ${heroData.powerstats.combat}`}
         </Button>
         <Button
           sx={{ backgroundColor: "purple", marginBottom: 2 }}
           variant="contained"
+          value={heroData.powerstats.durability}
+          onClick={handleClick}
         >
           {`Durability: ${heroData.powerstats.durability}`}
         </Button>
         <Button
           sx={{ backgroundColor: "purple", marginBottom: 2 }}
           variant="contained"
+          value={heroData.powerstats.intelligence}
+          onClick={handleClick}
         >
           {`Intelligence: ${heroData.powerstats.intelligence}`}
         </Button>
         <Button
           sx={{ backgroundColor: "purple", marginBottom: 2 }}
           variant="contained"
+          value={heroData.powerstats.power}
+          onClick={handleClick}
         >
           {`Power: ${heroData.powerstats.power}`}
         </Button>
         <Button
           sx={{ backgroundColor: "purple", marginBottom: 2 }}
           variant="contained"
+          value={heroData.powerstats.speed}
+          onClick={handleClick}
         >
           {`Speed: ${heroData.powerstats.speed}`}
         </Button>
         <Button
           sx={{ backgroundColor: "purple", marginBottom: 2 }}
           variant="contained"
+          value={heroData.powerstats.strength}
+          onClick={handleClick}
         >
           {`Strength: ${heroData.powerstats.strength}`}
         </Button>
